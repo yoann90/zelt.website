90488ed806ef5c1830f701e0cf32d4f3792c53cc
yoann <sayo97278@hotmail.fr>
2024-05-02 17:43:57 +0200

    fix padding bug

--- index.js
+++ index.js
@@ -243,9 +243,11 @@ ScrollTrigger.create({
 });
 
 function openNav() {
-  document.getElementById("myNav").style.width = "100%";
+  document.getElementById("myNav").style.height = "100%";
 }
 
 function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+  document.getElementById("myNav").style.height = "0%";
 }
+
+
